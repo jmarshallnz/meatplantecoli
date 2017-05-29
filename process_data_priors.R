4cecc4d98ba91707d0325c9f26a35b3801e37c1e
Jonathan Marshall <jmarshall@never.you.mind>
2017-05-30 08:46:18 +1200

    updates prior processing to final

--- process_data_priors.R
+++ process_data_priors.R
@@ -17,6 +17,11 @@ copy_end   <- c(vals[-1]-1, nrow(dat))
 for (i in seq_along(vals)) 
   dat[copy_start[i]:copy_end[i],1] <- dat[vals[i],1]
 dat = dat[-vals,]
+
+# the other datasheet didn't have separate unloading ramps/pens
+ramps <- c(FALSE, dat[-1,1] == "Unloading Ramps & Pens")
+dat[ramps,1] = "Plant details"
+
 dat$Variable = apply(dat[,1:2], 1, paste, collapse=":")
 # Capitalise anything after a space
 simplify_var <- function(x) {
@@ -49,10 +54,39 @@ plants = apply(plants, 2, function(x) { str_replace_all(x, " ", "") })
 plants[plants == "na"] = NA
 
 library(dplyr)
-final = data.frame(plants[,c(5,1:4)])
-names(final) <- c("Variable", "Likely", "Neutral", "Unlikely", "Unknown")
+final = data.frame(plants[,c(5,1:4)], stringsAsFactors = FALSE)
+names(final) <- c("Variable", "LikelyIncrease", "LikelyDecrease", "NoEffect", "Unknown")
 final[,2:5] <- lapply(final[,2:5], function(x) { as.numeric(as.character(x)) })
 rowSums(final[,2:5], na.rm=TRUE)
 
+#redo the variables as needed
+final$Variable <- str_replace_all(final$Variable, "[^a-zA-Z0-9]",".")
+
+# Fixups
+final$Variable <- str_replace(final$Variable, "NotShortenedBeforeHidePuller", "LeftUnshortenedBeforeHidePuller")
+final$Variable <- str_replace(final$Variable, "MethodUsedI.e.Knife.AirknifeOrRoller", "MethodUsedI.e.FlayingByKnifeOrAirknifeOrByRoller")
+final$Variable <- str_replace(final$Variable, "PizzleOccludedBeforeHidePuller", "PizzleOccludedBeforeHidePulle")
+final$Variable <- str_replace(final$Variable, "WaterContact.CabinetWashPriorToChiller", "WaterContact.CabinetWashPriorToChiller.BR")
+final$Variable <- str_replace(final$Variable, "Flaying.OperatorFollowingCarcass", "Flaying.OperatorFollowingCarcass.StationaryOrBoth")
+final$Variable <- str_replace(final$Variable, "OpeningUp.TypeOfOpeningCutUsed", "OpeningUp..7.OpeningCutUsed")
+
+# Likely variables are combined (merged cells FTW???!!?), so combine them
+final <- final %>% mutate(Effect = rowSums(cbind(LikelyIncrease,LikelyDecrease),na.rm=TRUE)) %>% select(-LikelyIncrease,-LikelyDecrease)
+
+# get rid of NAs
+final[is.na(final)] <- 0
+
 # write file
 write.csv(final, "data/final_prior_data.csv", row.names = FALSE)
+
+
+# test we have all the variables we want
+dat <- read_csv("data/final_data.csv")
+
+
+missing1 <- !names(dat) %in% final$Variable
+missing2 <- !final$Variable %in% names(dat)
+names(dat)[missing1]
+final$Variable[missing2]
+
+
