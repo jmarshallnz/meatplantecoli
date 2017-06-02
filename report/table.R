4facacdd16dc3ab4cb6d43f7864bd39423bdd65e
Jonathan Marshall <jmarshall@never.you.mind>
2017-06-02 16:40:25 +1200

    adds report

---
+++ report/table.R
@@ -0,0 +1,47 @@
+library(dplyr)
+
+#' Read in the data
+plants = read.csv("data/final_data.csv")
+priors = read.csv("data/final_prior_data.csv")
+
+# Days is missing for plant 9
+plants <- plants[plants$Plant != 'Plant9',]
+
+# ignore variables iwth missing data for now
+# ignore variables which have no variance
+missing <- apply(plants[,1:146], 2, function(x) { sum(is.na(x)) }) > 0
+no_variance <- apply(plants[,1:146], 2, function(x) { length(unique(x))}) < 2
+
+# table up the names of the plants along with the various functions they have
+vars <- plants[,1:146]
+nvar <- names(vars)
+ovar <- unlist(lapply(seq_along(nvar), function(x) { paste(sort(unique(plants[,x])), collapse=', ') }))
+
+priors <- priors[match(nvar, priors$Variable),]
+
+# split up the variable names and add a space between capitals
+nvar <- gsub("Head\\.Tongue", "Head & Tongue", nvar)
+nvar <- gsub("Shoulder\\.NeckClearing\\.Flaying", "Shoulder & Neck clearing - flaying", nvar)
+nvar <- gsub("PullingSocks\\.ShouldersManually", "Pulling socks / shoulders manually", nvar)
+nvar <- gsub("Navel\\.Umbilicus", "Navel / Umbilicus", nvar)
+nvar <- gsub("Changeover\\.GambrelUpOperator", "Changeover / Gambrel Up Operator", nvar)
+nvar <- gsub("CarcassWashing.WaterContact", "CarcassWashing / WaterContact", nvar)
+nvar <- gsub("MethodUsedI.e.FlayingByKnifeOrAirknifeOrByRoller", "MethodUsed", nvar)
+nvar <- gsub("([a-z])([A-Z])", "\\1 \\2", nvar)
+
+nsplit <- strsplit(nvar, "\\.")
+ncat <- unlist(lapply(nsplit, function(x) { x[[1]] }))
+nsub <- unlist(lapply(nsplit, function(x) { paste(unlist(x[-1]), collapse=' ') }))
+
+d <- data.frame(Category=ncat, Variable=nsub,
+                Missing=ifelse(missing, "Yes", ""),
+                `No Variation`=ifelse(no_variance, "Yes", ""),
+                `Effect Likely`=priors$Effect,
+                `Effect Unlikely`=priors$NoEffect,
+                `Effect Unknown`=priors$Unknown,
+                 stringsAsFactors = FALSE, check.names = FALSE)
+
+# remove duplication in d (e.g. category column)
+d$Category[duplicated(d$Category)] <- ""
+
+write.csv(d, "temp/table.csv", row.names=FALSE)
