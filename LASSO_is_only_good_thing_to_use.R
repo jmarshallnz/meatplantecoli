f0d594a9fba31e1bfc9f9d2edbe4314b0b80e128
Jonathan Marshall <jmarshall@never.you.mind>
2017-06-12 11:35:37 +1200

    updates code. Needs a good cleanup

--- LASSO_is_only_good_thing_to_use.R
+++ LASSO_is_only_good_thing_to_use.R
@@ -170,3 +170,256 @@ b <- a %>% gather('penalty', 'value', `0`:`2`)
 b$lambda <- as.numeric(as.character(b$lambda))
 b$alpha <- as.numeric(as.character(b$alpha))
 write.csv(b, "temp/lasso.csv", row.names=FALSE)
+
+
+
+
+
+
+
+
+
+############ FINDING BEST MODEL FIT ################
+
+# grab the foldid
+set.seed(2)
+lambda <- 10^(-seq(1,4, by=0.1))
+cvfit <- cv.glmnet(modelmat, outcome, family="binomial", type.measure='auc', lambda=lambda, nfolds=50, alpha=1,keep=TRUE)
+foldid <- cvfit$foldid
+
+# now do the fitting - coarser than above as it takes ages
+foldid <- as.numeric(newplant$Plant)
+reps <- 100
+alpha <- seq(0,1,by=0.2)
+penalty <- seq(0,2,by=0.4)
+cvm <- array(NA, dim = c(reps,length(lambda), length(alpha), length(penalty)))
+dimnames(cvm) <- list(1:reps, as.character(lambda), as.character(alpha), as.character(penalty))
+for (a in seq_along(alpha)) {
+  #  lambda <- matrix(NA, 100, length(penalty))
+  for (p in seq_along(penalty)) {
+    # compute penalty
+    cat("Fitting model for penalty", p, "alpha", a, "\n")
+    penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * penalty[p])
+    penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+    # CV fit model, and pull out coefficients at lambda min
+    cvm_ap <- matrix(0, reps, length(lambda))
+    for (i in 1:reps) {
+      cat("Rep", i, "of", reps, "\n")
+      cvfit <- cv.glmnet(modelmat, outcome, family="binomial", type.measure='auc', lambda=lambda, alpha=alpha[a], penalty.factor=penalty.factor, foldid=foldid)
+      cvm[i,,a,p] <- cvfit$cvm
+    }
+  }
+}
+
+# compress the array down to something smaller
+all <- list()
+k <- 1
+for (i in 1:dim(cvm)[1]) {
+  for (j in 1:dim(cvm)[2]) {
+    cat("Up to k=", k, "of", prod(dim(cvm)[1:2]), "\n")
+    all[[k]] <- data.frame(Rep=dimnames(cvm)[[1]][i], lambda=dimnames(cvm)[[2]][j], alpha=dimnames(cvm)[[3]], cvm[i,j,,], check.names = FALSE)
+    k <- k + 1
+  }
+}
+
+a <- do.call(rbind, all)
+library(tidyr)
+b <- a %>% gather('penalty', 'value', `0`:`2`)
+b$lambda <- as.numeric(as.character(b$lambda))
+b$alpha <- as.numeric(as.character(b$alpha))
+b$penalty <- as.numeric(as.character(b$penalty))
+b$Rep <- as.numeric(as.character(b$Rep))
+
+s <- b %>% group_by(lambda, alpha, penalty) %>% summarize(mvalue = median(value),
+                                                          lvalue = quantile(value,0.2),
+                                                          uvalue = quantile(value,0.8)) %>%
+  ungroup()
+
+write.csv(s, "temp/lasso_fit_LOO.csv", row.names=FALSE)
+# compute summary dataframe
+
+ggplot(s) + geom_ribbon(aes(x=log10(lambda), ymin=lvalue, ymax=uvalue), alpha=0.5, fill='steelblue') + 
+  geom_line(aes(x=log10(lambda), y=mvalue)) + facet_grid(alpha ~ penalty) +
+  theme_bw() +
+  ylab("Predictive performance (Area under curve)") +
+  xlab("LASSO/Ridge penalty")
+
+# find the best
+s[which.max(s$mvalue),]
+s[which.max(s$lvalue),]
+s[which.max(s$uvalue),]
+
+s %>% top_n(20, mvalue) %>% arrange(mvalue)
+
+
+# seems to be around alpha = 0.7..1.0, lambda = 0.03..0.05, penalty=2
+# now do the fitting - coarser than above as it takes ages
+set.seed(2)
+lambda <- 10^(-seq(1,2, by=0.025))
+cvfit <- cv.glmnet(modelmat, outcome, family="binomial", type.measure='auc', lambda=lambda, nfolds=50, alpha=1,keep=TRUE)
+foldid <- cvfit$foldid
+
+reps <- 100
+alpha <- seq(0.4,0.7,by=0.05) #10
+penalty <- seq(0.9,1.1,by=0.05)  #5
+cvm <- array(NA, dim = c(reps,length(lambda), length(alpha), length(penalty)))
+dimnames(cvm) <- list(1:reps, as.character(lambda), as.character(alpha), as.character(penalty))
+for (a in seq_along(alpha)) {
+  #  lambda <- matrix(NA, 100, length(penalty))
+  for (p in seq_along(penalty)) {
+    # compute penalty
+    cat("Fitting model for penalty", p, "alpha", a, "\n")
+    penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * penalty[p])
+    penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+    # CV fit model, and pull out coefficients at lambda min
+    cvm_ap <- matrix(0, reps, length(lambda))
+    for (i in 1:reps) {
+      cat("Rep", i, "of", reps, "\n")
+      cvfit <- cv.glmnet(modelmat, outcome, family="binomial", type.measure='auc', nfolds=50, lambda=lambda, alpha=alpha[a], penalty.factor=penalty.factor, foldid=foldid)
+      cvm[i,,a,p] <- cvfit$cvm
+    }
+  }
+}
+
+# compress the array down to something smaller
+all <- list()
+k <- 1
+for (i in 1:dim(cvm)[1]) {
+  for (j in 1:dim(cvm)[2]) {
+    cat("Up to k=", k, "of", prod(dim(cvm)[1:2]), "\n")
+    all[[k]] <- data.frame(Rep=dimnames(cvm)[[1]][i], lambda=dimnames(cvm)[[2]][j], alpha=dimnames(cvm)[[3]], cvm[i,j,,], check.names = FALSE)
+    k <- k + 1
+  }
+}
+
+a <- do.call(rbind, all)
+library(tidyr)
+b <- a %>% gather('penalty', 'value', `0.9`:`1.1`)
+b$lambda <- as.numeric(as.character(b$lambda))
+b$alpha <- as.numeric(as.character(b$alpha))
+b$penalty <- as.numeric(as.character(b$penalty))
+b$Rep <- as.numeric(as.character(b$Rep))
+
+s <- b %>% group_by(lambda, alpha, penalty) %>% summarize(mvalue = median(value),
+                                                          lvalue = quantile(value,0.2),
+                                                          uvalue = quantile(value,0.8)) %>%
+  ungroup()
+
+write.csv(s, "temp/lasso_fit_zoom.csv", row.names=FALSE)
+s <- read.csv("temp/lasso_fit_zoom.csv")
+best_fit <- s %>% top_n(1, mvalue) %>% arrange(mvalue)
+ggplot(s) + geom_ribbon(aes(x=log10(lambda), ymin=lvalue, ymax=uvalue), alpha=0.5, fill='steelblue') + 
+  geom_line(aes(x=log10(lambda), y=mvalue)) + facet_grid(alpha ~ penalty) +
+  theme_bw() +
+  ylab("Predictive performance (Area under curve)") +
+  xlab("LASSO/Ridge penalty")
+
+# try the 'best' model
+penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * best_fit$penalty)
+penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+mod <- glmnet(modelmat, outcome, family="binomial", lambda=best_fit$lambda, alpha=best_fit$alpha, penalty.factor=penalty.factor)
+co <- as.matrix(coef(mod))
+pred <- predict(mod, newx=modelmat, type='response')
+
+# try the other 'best' model
+s <- read.csv("temp/lasso_fit_zoomed.csv")
+best_fit2 <- s %>% top_n(1, mvalue) %>% arrange(mvalue)
+penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * best_fit2$penalty)
+penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+mod <- glmnet(modelmat, outcome, family="binomial", lambda=best_fit2$lambda, alpha=best_fit2$alpha, penalty.factor=penalty.factor)
+co2 <- as.matrix(coef(mod))
+co <- data.frame(VarName=rownames(co), Coefficient=co[,1], Coef2=co2[,1])
+write.csv(co, "temp/final_coef.csv", row.names=FALSE)
+pred2 <- predict(mod, newx=modelmat, type='response')
+pred <- data.frame(Plant = newplant$Plant, STEC=as.numeric(as.character(outcome)), Prediction=as.numeric(pred), Prediction2=as.numeric(pred2))
+
+# try prediction
+d <- pred %>% group_by(Plant) %>% summarize(Observed=mean(STEC), Optimal=unique(Prediction), Alternate=unique(Prediction2)) %>% gather('Model', 'Predicted', Optimal:Alternate)
+write.csv(d, "temp/final_prediction.csv", row.names=FALSE)
+
+plot(Predicted ~ Observed, data=d, xlim=c(0,0.35), ylim=c(0,0.35))
+abline(0,1)
+
+# now fit the model with these parameters
+penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * 2)
+penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+mod <- glmnet(modelmat, outcome, family="binomial", alpha=0.925, penalty.factor=penalty.factor)
+c <- as.matrix(coef(mod, s=0.03162278, exact=TRUE))
+
+pred <- predict(mod, newx=modelmat, s=0.03162278, exact=TRUE, type='response')
+pred <- data.frame(Plant = newplant$Plant, STEC=as.numeric(as.character(outcome)), Prediction=as.numeric(pred))
+d <- pred %>% group_by(Plant) %>% summarize(Observed=mean(STEC), Predicted=unique(Prediction))
+
+plot(Predicted ~ Observed, data=d, xlim=c(0,0.35), ylim=c(0,0.35))
+abline(0,1)
+
+# bootstrap some confidence intervals...
+covars <- which(c != 0)[-1]
+reps <- 1000
+coef <- matrix(NA, reps, length(covars)+1) #intercept
+i <- 1
+while (i <= reps) {
+  # TODO: Sample with replacement within plants
+  cat("Iteration", i, "of", reps, "\n")
+  s <- sample(1:nrow(modelmat), nrow(modelmat), replace=TRUE)
+  # check we have enough in each class
+  mod <- glmnet(modelmat[s,covars], outcome[s], family="binomial", alpha=0.925, penalty.factor=penalty.factor[covars])
+  co <- as.matrix(coef(mod))
+  coSums <- colSums(co != 0)
+  wch <- which.max(coSums)
+  if (coSums[wch] == ncol(coef)) {
+    coef[i,] <- as.numeric(co[,wch])
+    i <- i + 1
+  }
+}
+
+apply(coef, 2, function(x) { sum(x != 0) })
+
+mod <- glmnet(modelmat[,covars], outcome, family="binomial", alpha=0.925, penalty.factor=penalty.factor[covars])
+co <- as.matrix(coef(mod))
+
+# hmm, build model containing only these covariates??
+
+# This works really badly, as it's choosing covariates that are not really that useful???
+penalty.factor = 2^((priors$NoEffect - priors$Effect)/5 * 1)
+penalty.factor <- penalty.factor[attr(modelmat, 'assign')]
+mod <- glmnet(modelmat, outcome, family="binomial", alpha=0.5, penalty.factor=penalty.factor)
+c <- as.matrix(coef(mod, s=10^(-1.6), exact=TRUE))
+which(c[,1] != 0)
+
+covars <- which(c != 0)[-1]
+reps <- 1000
+coef <- matrix(NA, reps, length(covars)+1) #intercept
+i <- 1
+
+plants <- as.numeric(newplant$Plant)
+mp <- max(plants)
+
+while (i <= reps) {
+  # TODO: Sample with replacement within plants
+  cat("Iteration", i, "of", reps, "\n")
+  w <- sample(1:mp, mp, replace=TRUE)
+  s <- NULL
+  for (j in seq_along(w)) {
+    s <- c(s, which(plants == w[j]))
+  }
+  # check we have enough in each class
+  mod <- glmnet(modelmat[s,covars], outcome[s], family="binomial", lambda=10^(-1.6), alpha=0.925, penalty.factor=penalty.factor[covars])
+  co <- as.matrix(coef(mod))
+#  coSums <- colSums(co != 0)
+#  wch <- which.max(coSums)
+#  if (coSums[wch] == ncol(coef)) {
+    coef[i,] <- as.numeric(co[,1])
+    i <- i + 1
+#  }
+}
+
+apply(coef, 2, function(x) { sum(x != 0) })
+apply(coef, 2, quantile, c(0.025, 0.975))
+apply(coef, 2, sd)
+
+apply(coef, 2, mean)
+
+# Hmm, not really sure what to do: there are essentially arbitrarily large no of models that
+# give the same fit to the data. It doesn't make much sense to try and limit it, nor to try
+# and get 
